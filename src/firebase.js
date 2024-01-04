a9937054ea13d681b2f08cb07643503494676116
sandip parida <sandipparida672@gmail.com>
2024-01-04 19:08:56 +0530

    ..

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import {getFirestore} from "firebase/firestore"
 import {getStorage} from "firebase/storage"
  
 const firebaseConfig = {
-  apiKey: "AIzaSyD3EPfsa714kImzHg9aY04KHFTvjuLFmkM",
-  authDomain: "firechat-584fa.firebaseapp.com",
-  projectId: "firechat-584fa",
-  storageBucket: "firechat-584fa.appspot.com",
-  messagingSenderId: "427071833627",
-  appId: "1:427071833627:web:f050cc69f5a5d09fe09abe"
+  apiKey: "AIzaSyAc4go5teVt1qQ3d9HkowJDJKHmVBhmJkk",
+authDomain: "fir-chat-app-v1-f2ad2.firebaseapp.com",
+projectId: "fir-chat-app-v1-f2ad2",
+storageBucket: "fir-chat-app-v1-f2ad2.appspot.com",
+messagingSenderId: "554414972250",
+appId: "1:554414972250:web:6b96e427b25ee45bbb2763"
+  
 };
 
 // Initialize Firebase
@@ -17,3 +18,19 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
 export const db = getFirestore()
 export const storage = getStorage();
+
+
+// apiKey: "AIzaSyAc4go5teVt1qQ3d9HkowJDJKHmVBhmJkk",
+// authDomain: "fir-chat-app-v1-f2ad2.firebaseapp.com",
+// projectId: "fir-chat-app-v1-f2ad2",
+// storageBucket: "fir-chat-app-v1-f2ad2.appspot.com",
+// messagingSenderId: "554414972250",
+// appId: "1:554414972250:web:6b96e427b25ee45bbb2763"
+
+
+// apiKey: "AIzaSyD3EPfsa714kImzHg9aY04KHFTvjuLFmkM",
+//   authDomain: "firechat-584fa.firebaseapp.com",
+//   projectId: "firechat-584fa",
+//   storageBucket: "firechat-584fa.appspot.com",
+//   messagingSenderId: "427071833627",
+//   appId: "1:427071833627:web:f050cc69f5a5d09fe09abe"
